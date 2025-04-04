5630207e50b446e98caf817f3a0161d479dc0f64
Mokles_Bhai <hanzalanuhan33@gmail.com>
2025-04-04 21:52:27 +0600

    Update Timer_Watch.cpp
    This C++ program is a customizable countdown timer that:
    
    1. Takes user input for hours, minutes, and seconds.
    2. Calculates the total time in seconds.
    3. Displays the countdown in a live-updating format like HH:MM:SS, using proper formatting with setfill and setw.
    4. Uses multithreading (sleep_for) to handle real-time countdown updates.
    5. Cleans up the terminal using system("clear") to keep things looking fresh.
    6. Displays a funny Bengali message when the timer ends.

--- Timer_Watch.cpp
+++ Timer_Watch.cpp
@@ -1,1 +1,73 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include<iomanip>
+using namespace std;
+using namespace std::this_thread;
+using namespace std::chrono;
 
+int digits(int n)
+{
+    int count = 0;
+    n = abs(n);
+    if(n == 0) return 1;
+    while(n!=0){
+        n /= 10;
+        count++;
+    }
+    return count;
+}
+
+void timer(int hour, int min, int sec)
+{
+
+    int total_sec = (hour*60*60) + (min*60) + sec;
+    while(total_sec >= 0)
+    {
+        int curr_hr = total_sec / 3600;
+        int h_dig = digits(curr_hr);
+
+        int curr_min = (total_sec % 3600) /60;
+        int  m_dig = digits(curr_min);
+
+        int curr_sec = total_sec % 60;
+        
+        cout << "\r"<< setw(h_dig) << setfill('0') << curr_hr << ":"<< setw(m_dig) << setfill('0') << curr_min  << ":"<< setw(2) << setfill('0') << curr_sec  << flush;
+
+        sleep_for(seconds(1));
+        total_sec --;
+    }
+    cout << "\n...⏳..." ;
+    sleep_for(milliseconds(1500));
+    cout << "\n🛑 Somoy sesh hoye gese! Hahhaha 😂" << endl;
+}
+
+int main()
+{
+    system("clear");
+    int hour, min, sec;
+    cout << "Enter Hours: ";
+    cin >> hour;
+
+    system("clear");
+
+    cout << "Enter Minutes: ";
+    cin >> min;
+    system("clear");
+
+    cout << "Enter Seconds: ";
+    cin >> sec;
+    system("clear");
+
+    if(hour >= 0 && min >=0 && sec >=0 ){
+        cout << "Timer Set for ";
+        if(hour > 0 ) cout << hour << " hour"  << (hour > 1 ? "s ":" ");
+        if(min > 0) cout << min << " min" << (min > 1 ? "s ":" ");
+        if(sec > 0) cout << sec << " sec" <<(sec > 1 ? " s":" ");
+        cout << endl;
+        timer(hour, min, sec);
+    }
+    else{
+        cout <<"Sorry Time Can't Be Negative"<<endl;
+    }
+}
