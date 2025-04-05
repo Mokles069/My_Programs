7e3a56328fd3b079474f43aaa4315a0329992b8b
Mokles_Bhai <hanzalanuhan33@gmail.com>
2025-04-05 07:18:15 +0600

    Update Fancy_Word.cpp

--- Fancy_Word.cpp
+++ Fancy_Word.cpp
@@ -1,1 +1,39 @@
+#include<iostream>
+#include<string>
+#include<thread>
+#include<chrono>
+#include<cctype>
+using namespace std;
+using namespace std::this_thread;
+using namespace std::chrono;
 
+int main()
+{
+    system("clear");
+    string massage;
+    cout << "Enter a Sentence/word: ";
+    getline(cin, massage);
+
+    string another;
+
+    for(char c: massage){
+        if(!isalpha(c)){
+            another.push_back(c);
+            cout << c ;
+            continue;
+        }
+    char start = islower(c) ? 'a' : 'A';
+    string animate = "";
+
+    for(char letter = start; letter<=c; letter++)
+    {
+        animate = letter;
+        cout << "\r" << another << animate << flush;
+        sleep_for(milliseconds(5));
+    }
+    another.push_back(c);
+    cout << "\n";
+    }
+
+    cout << "☺☺☺☺" << endl;
+}
